28f2aeab6c0c5630c9760a4c7d5ebf2794ae06d5
digitals031064 <digitals031064@gmail.com>
2025-03-30 00:52:51 +0800

    Update Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,14 @@ class UserSeeder extends Seeder
             'password' => Hash::make('rootroot'), // Encrypting the password
         ]);
 
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@ad',
+            'usertype' => 'admin',
+            'password' => Hash::make('rootroot'), // Encrypting the password
+        ]);
+        
+
                 // Create sample users with Faker
         $faker = Faker::create();
 
