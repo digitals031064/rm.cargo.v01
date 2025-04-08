d03b60157005987bb27d1d450f209e938dc692fa
maynardcalingacion <180412@adzu.edu.ph>
2025-04-08 21:56:33 +0800

    set update modal waybill_no to read only

--- resources/views/waybill/waybills.blade.php
+++ resources/views/waybill/waybills.blade.php
@@ -288,7 +288,7 @@
                             <input type="hidden" id="update_waybill_id">
                             <div>
                                 <label for="waybill_no" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waybill Number</label>
-                                <input type="text" name="waybill_no" id="update_waybill_no" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Waybill Number"  required=""> 
+                                <input type="text" name="waybill_no" id="update_waybill_no" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Waybill Number"  required="" readonly> 
                             </div>
                             
                             <div class="grid gap-4 mb-6 sm:grid-cols-2">
