a3d027a20ecc0f8a7ec4dadc6520f913166cc43b
maynardcalingacion <180412@adzu.edu.ph>
2025-04-11 14:56:03 +0800

    Update waybills.blade.php

--- resources/views/waybill/waybills.blade.php
+++ resources/views/waybill/waybills.blade.php
@@ -183,7 +183,7 @@
                             <div>
                                 <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waybill Type</label>
                                 <select name="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                                    <option value="default">Default</option>
+                                    <option value="default">Manila</option>
                                     <option value="cebu">Cebu</option>
                                 </select>
                             </div>
@@ -313,7 +313,7 @@
                                 <div>
                                     <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waybill Type</label>
                                     <select name="type" id="update_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                                        <option value="default">Default</option>
+                                        <option value="default">Manila</option>
                                         <option value="cebu">Cebu</option>
                                     </select>
                                 </div>
